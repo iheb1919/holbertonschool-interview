2634648d37df881c092c153e98017e4edb909acf
iheb1919 <ihebmejri14@gmail.com>
2020-08-18 22:51:39 +0100

    fix1

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -1,4 +1,7 @@
-#ifndef sandpiles_H
-#define sandpiles_H
+#ifndef SANDPILES_H
+#define SANDPILES_H
+
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+void tople(int grid1[3][3]);
+int stable(int grid1[3][3]);
 #endif
