6561624b034bb44362d8f973ff3391d527b25268
iheb1919 <ihebmejri14@gmail.com>
2020-08-24 14:02:35 +0100

    try0

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,57 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * size - function return the size of the list
+ * @head: the list
+ * Return: the size
+ */
+
+int size(listint_t **head)
+{
+	listint_t *new = *head;
+	int i = 0;
+
+	while (new)
+	{
+		i++;
+		new = new->next;
+	}
+	return (i);
+
+}
+
+/**
+ * is_palindrome - function that verify if the list is palidrome or not
+ * @head: the list
+ * Return: 1 if palidrome 0 if not.
+ */
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *new;
+	int i = 0, s = 0;
+	int *array;
+
+	if (!head)
+		return (0);
+	if (!(*head) || !(*head)->next)
+		return (1);
+	new = *head;
+	s = size(head);
+	array = malloc(sizeof(int) * s);
+	while (new)
+	{
+		array[i] = new->n;
+		i++;
+		new = new->next;
+	}
+	for (i = 0; i < s / 2; i++)
+	{
+		if (array[i] != array[s - 1 - i])
+			return (0);
+	}
+	return (1);
+}
