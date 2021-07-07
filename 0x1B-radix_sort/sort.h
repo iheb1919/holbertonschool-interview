a8c3c4d3b8959c94fe286e8b1fb07f32de3934f4
iheb1919 <root@LAPTOP-2RTLC8EF.localdomain>
2021-07-08 01:58:21 +0200

    try

--- 0x1B-radix_sort/sort.h
+++ 0x1B-radix_sort/sort.h
@@ -6,9 +6,8 @@
 
 void radix_sort(int *array, size_t size);
 
-int getMax(int array[], int n);
+int get_max(int *array, size_t size);
 
-void countingSort(int array[], int size, int place);
 
 void print_array(const int *array, size_t size);
 #endif
