ab408ba9d85ec4c5e39396f95df35b1034d1ebba
iheb1919 <root@LAPTOP-2RTLC8EF.localdomain>
2021-07-14 22:15:55 +0200

    try

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -7,9 +7,10 @@ def island_perimeter(grid):
     """island_perimeter
     """
     s = 0
-
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
+    n = len(grid)
+    m = len(grid[0])
+    for i in range(n):
+        for j in range(m):
             if (grid[i][j] == 1):
                 if ((i - 1) >= 0 and grid[i - 1][j] == 0):
                     s += 1
