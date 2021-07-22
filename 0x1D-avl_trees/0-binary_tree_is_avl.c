51315f268e9b6cb7a792211b9d8d1dde279e3516
iheb1919 <root@LAPTOP-2RTLC8EF.localdomain>
2021-07-22 23:39:25 +0200

    kk

--- 0x1D-avl_trees/0-binary_tree_is_avl.c
+++ 0x1D-avl_trees/0-binary_tree_is_avl.c
@@ -5,14 +5,17 @@
  * @tree: tree
  * Return: 1 or 0
  */
-int avl_node_height( binary_tree_t *tree ) {
-	int height_left = 0;
-	int height_right = 0;
+int avl_node_height(binary_tree_t *tree)
+{
+int height_left = 0;
+int height_right = 0;
 
-	if( tree->left ) height_left = avl_node_height( tree->left );
-	if( tree->right ) height_right = avl_node_height( tree->right );
+if (tree->left)
+height_left = avl_node_height(tree->left);
+if (tree->right)
+height_right = avl_node_height(tree->right);
 
-	return height_right > height_left ? ++height_right : ++height_left;
+return (height_right > height_left ? ++height_right : ++height_left);
 }
 
 /**
@@ -20,13 +23,16 @@ int avl_node_height( binary_tree_t *tree ) {
  * @tree: tree
  * Return: 1 or 0
  */
-int binary_tree_is_avl(const binary_tree_t *tree) {
-	int balance = 0;
-	if( tree->left  ) balance += avl_node_height( tree->left );
-	if( tree->right ) balance -= avl_node_height( tree->right );
-    if (balance <= 1)
-    return 1;
-	return (0);
+int binary_tree_is_avl(const binary_tree_t *tree)
+{
+int balance = 0;
+if (tree->left)
+balance += avl_node_height(tree->left);
+if (tree->right)
+balance -= avl_node_height(tree->right);
+if (balance <= 1)
+return (1);
+return (0);
 }
 
 
