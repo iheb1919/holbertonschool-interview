97edbba096457a8ed4bbb395c229edf3a509da25
iheb1919 <root@LAPTOP-2RTLC8EF.localdomain>
2021-09-23 03:46:15 +0200

    ll

--- 0x1F-pascal_triangle/0-pascal_triangle.py
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -5,23 +5,18 @@
 
 
 def pascal_triangle(n):
-    r = [[1]]
-    sum = []
-    x = 0
-    y = 0
-    if (n <= 0):
-        return (sum)
-    for k in range(n - 1):
-        r[x].insert(0, 0)
-        r[x].append(0)
-        ra = len(r[x])
-        for i in range(ra - 1):
-            sum.append(r[x][y] + r[x][y + 1])
-            y = y + 1
-        r[x].pop(0)
-        r[x].pop(-1)
-        r.append(sum)
-        x = x + 1
-        y = 0
-        sum = []
-    return (r)
+    """pascal_triangle"""
+    if n <= 0:
+        return []
+
+    pascal = [[1]]
+    for i in range(1, n):
+        row = [1]
+        prev = pascal[i - 1]
+        for j in range(len(prev)):
+            new = prev[j] + prev[j + 1] if j != len(prev) - 1 else 1
+            row.append(new)
+
+        pascal.append(row)
+
+    return pascal
