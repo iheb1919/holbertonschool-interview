e019df39d1783e159d17cac950d248fb705187e5
iheb1919 <root@LAPTOP-2RTLC8EF.localdomain>
2021-09-23 03:39:21 +0200

    rr

--- 0x1D-avl_trees/0-binary_tree_is_avl.c
+++ 0x1D-avl_trees/0-binary_tree_is_avl.c
@@ -1,39 +1,85 @@
+   
 #include "binary_trees.h"
 
 /**
- * avl_node_height - enter
- * @tree: tree
- * Return: 1 or 0
+ * tree_height - Calculates the height of a binary tree.
+ *
+ * @tree: Tree to calculate height [const binary_tree_t].
+ *
+ * Return: Height of the binary tree [size_t].
  */
-int avl_node_height(binary_tree_t *tree)
+size_t tree_height(const binary_tree_t *tree)
 {
-int height_left = 0;
-int height_right = 0;
+	size_t right = 0, left = 0;
 
-if (tree->left)
-height_left = avl_node_height(tree->left);
-if (tree->right)
-height_right = avl_node_height(tree->right);
-
-return (height_right > height_left ? ++height_right : ++height_left);
+	if (tree == NULL)
+		return (0);
+	if (tree->right)
+		right += tree_height(tree->right) + 1;
+	if (tree->left)
+		left += tree_height(tree->left) + 1;
+	if (left > right)
+		return (left);
+	return (right);
 }
 
 /**
- * binary_tree_is_avl - enter
- * @tree: tree
- * Return: 1 or 0
+ * avl_tree - Checks if a tree is AVL.
+ *
+ * @tree: Address of the tree [const binary_tree_t].
+ * @max: Max limit [int].
+ * @min: Min limit [int].
+ *
+ * Return: [size_t] number:
+ *         -> 1 if tree is AVL.
+ *         -> 0 if tree is not AVL.
+ *
+ */
+size_t avl_tree(const binary_tree_t *tree, int max, int min)
+{
+	/** ls: Left Side | rs: Right Side. */
+	size_t ls = tree_height(tree->left), rs = tree_height(tree->right);
+
+	if (tree == NULL)
+		return (0);
+
+	if (ls > rs)
+		ls = ls - rs;
+	else
+		ls = rs - ls;
+	if (ls > 0)
+		return (0);
+
+	ls = 1;
+	rs = 1;
+
+	if (tree->left)
+	{
+		if ((tree->left->n >= tree->n) || (tree->left->n <= min))
+			return (0);
+		ls = avl_tree(tree->left, tree->n, min);
+	}
+	if (tree->right)
+	{
+		if ((tree->right->n <= tree->n) || (tree->right->n >= max))
+			return (0);
+		rs = avl_tree(tree->right, max, tree->n);
+	}
+	return (rs == 1 && ls == 1);
+}
+
+/**
+ * binary_tree_is_avl - Checks if a binary tree is a valid AVL Tree.
+ *
+ * @tree: Pointer to root of a binary tree [const binary_tree_t].
+ *
+ * Return: [int] number:
+ *         -> 1 if tree is AVL.
+ *         -> 0 if tree is not AVL.
  */
 int binary_tree_is_avl(const binary_tree_t *tree)
 {
-int balance = 0;
-if (tree->left)
-balance += avl_node_height(tree->left);
-if (tree->right)
-balance -= avl_node_height(tree->right);
-if (balance <= 1)
-return (1);
-return (0);
+	if (tree == NULL)
+		return (0);
+	return (avl_tree(tree, INT_MAX, INT_MIN));
 }
-
-
-
--- 0x1D-avl_trees/binary_trees.h
+++ 0x1D-avl_trees/binary_trees.h
@@ -1,10 +1,8 @@
-#ifndef _BINARY_TREES_H_
-#define _BINARY_TREES_H_
-
-#include <stddef.h>
-#include <stdlib.h>
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <limits.h>
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -13,17 +11,16 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-typedef struct binary_tree_s
+struct binary_tree_s
 {
 	int n;
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-} binary_tree_t;
+};
+typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s avl_t;
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-void binary_tree_print(const binary_tree_t *);
 int binary_tree_is_avl(const binary_tree_t *tree);
-
-
-#endif /* _BINARY_TREES_H_ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+void binary_tree_print(const binary_tree_t *tree);
+#endif
